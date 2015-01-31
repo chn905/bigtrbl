c38303235679b6ebc2ff902270ed92e4d15823c5
Chan Cho <verakorvo@gmail.com>
2015-01-31 02:12:21 -0800

    added following 8 models to webzin/models.py: User, Category, Hashtag, Post, Post_Hashtag, Section, Section_Hashtag, Reply

--- webzin/models.py
+++ webzin/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class User:
+    pass
+
+class Category:
+    pass
+
+class Hashtag:
+    pass
+
+class Post:
+    pass
+
+class Post_Hashtag:
+    pass
+
+class Section:
+    pass
+
+class Section_Hashtag:
+    pass
+
+class Reply:
+    pass
